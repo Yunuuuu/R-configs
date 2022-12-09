ddab2acd6ab3bc76b162825dceb4c3ae30a8d3f6
yun <yunyunpp96@outlook.com>
2022-12-10 02:05:49 +0800

    set R environment firstly then other pkg-specific config

--- pkg_config.R
+++ pkg_config.R
@@ -19,7 +19,15 @@ set_envpath <- function(env_var, ...) {
 }
 conda_env_dir <- "~/anaconda3/envs" # nolint
 
+# R-release environment path -----------------------------
+# conda create --name R-release -c conda-forge r-base git radian
+conda_renv <- file.path(conda_env_dir, "R-release")
+set_envpath("PKG_CONFIG_PATH", conda_renv, "lib", "pkgconfig")
+
 # pkg-specific PKG_CONFIG_PATH ---------------------------
+# these packages are not compitable with current conda R environment, install
+# this package in another conda environment and add the corresponding
+# PKG_CONFIG_PATH 
 # gert environment variable
 # conda create -n libgit2 -c conda-forge libgit2
 libgit2 <- file.path(conda_env_dir, "libgit2")
@@ -36,7 +44,3 @@ set_envpath("PKG_CONFIG_PATH", harfbuzz, "lib", "pkgconfig")
 fribidi <- file.path(conda_env_dir, "fribidi")
 set_envpath("PKG_CONFIG_PATH", fribidi, "lib", "pkgconfig")
 
-# R-release environment path -----------------------------
-# conda create --name R-release -c conda-forge r-base git radian
-conda_renv <- file.path(conda_env_dir, "R-release")
-set_envpath("PKG_CONFIG_PATH", conda_renv, "lib", "pkgconfig")
